ce8478adcd49810aec114da993ac2781539b207a
Aleksandra <akrzeszewska16@wp.pl>
2023-05-10 17:29:13 +0200

    pobranie zip i rozpoczecie kodowania gry

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import React from "react";
+export default function Square() {
+  return (
+  <div>
+    <button className="square">X</button>
+    <button className="square"></button>
+    </div>);
+}
